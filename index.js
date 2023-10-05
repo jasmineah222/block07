50d2d45a3328410d84a30ee3c51f2062c3f27e14
jasmineah222 <jasmineah222@gmail.com>
2023-10-05 14:55:02 -0400

    code loaded

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-/* 
+/*
 for 10, 30-20
 for 40, 120*(1/3)
-for 39, 30+9 
+for 39, 30+9
 
 Actual layout
 
@@ -9,13 +9,10 @@ let unlock=30-20
 let this=120* (1/3)
 let combo=(57-47)*(39/13)+((9/3)+6)*/
 
-console.log(alert("You have recieved this message because you have been chosen to unlock a secret vault. Here is the secret combination."))
+let unlock = 30 - 20;
+let the = 120 * (1 / 3);
+let combo = (57 - 47) * (39 / 13) + ((9 / 3) + 6);
+console.log("Expected Result " + "10-40-39");
+console.log(unlock + "-" + the + "-" + combo);
+console.log(alert("You have recieved this message because you have been chosen to unlock a secret vault. Here is the secret combination:30 - 20,120 * (1 / 3),(57 - 47) * (39 / 13) + ((9 / 3) + 6))"));
 
-let unlock=30-20
-
-let the= 120*(1/3)
-
-let combo=(57-47)*(39/13)+((9/3)+6)
-
-console.log("Expected Result " + "10-40-39")
-console.log(unlock+"-"+the+"-"+combo)
