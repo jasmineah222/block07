af2d6ed86ede1b0ad095911625a12b55b46ef55c
jasmineah222 <jasmineah222@gmail.com>
2023-10-05 13:48:11 -0400

    "started code, need to run"

---
+++ index.js
@@ -0,0 +1,22 @@
+/* 
+for 10, 30-20
+for 40, 120*(1/3)
+for 39, 30+9 
+
+Actual layout
+
+let unlock=30-20
+let this=120* (1/3)
+let combo=30+9*/
+
+console.log("Expected Result "+"10-40-39")
+console.log(unlock+this+code)
+
+let unlock=30-20
+
+let this=120* (1/3)
+
+let combo=30+9
+
+console.log("Expected Result "+" "10-40-39")
+console.log(unlock+this+code)
