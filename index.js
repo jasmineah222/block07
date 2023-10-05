399c7c1faae6c820b353ac503372a2e294621c0c
jasmineah222 <jasmineah222@gmail.com>
2023-10-05 14:21:15 -0400

    code progress

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ Actual layout
 
 let unlock=30-20
 let this=120* (1/3)
-let combo=30+9*/
+let combo=(57-47)*(39/13)+((9/3)+6)*/
 
-console.log("Expected Result "+"10-40-39")
-console.log(unlock+this+code)
+console.log(alert("You have recieved this message because you have been chosen to unlock a secret vault. Here is the secret combination."))
 
 let unlock=30-20
 
-let this=120* (1/3)
+let the= 120*(1/3)
 
-let combo=30+9
+let combo=(57-47)*(39/13)+((9/3)+6)
 
-console.log("Expected Result "+" "10-40-39")
-console.log(unlock+this+code)
+console.log("Expected Result " + "10-40-39")
+console.log(unlock+"-"+the+"-"+combo)
